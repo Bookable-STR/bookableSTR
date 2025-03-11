9bff0400b1f31bdbc33369c07c52b5ed0f6815f9
Nnenna-udefi <miriamudefi@gmail.com>
2025-03-11 16:24:19 +0100

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ReactNode } from "react";
 import React from "react";
 import Footer from "../components/footer";
+import Head from "next/head";
 
 const quicksand = Quicksand({
   variable: "--font-quick-sand",
@@ -15,9 +16,18 @@ const nunito = Nunito({
 });
 
 export const metadata = {
-  title: "BookableSTR",
+  title: "BookableSTR - Direct Booking Websites",
   description:
     "BookableSTR builds high-converting, user-friendly websites that showcase your properties, drive direct bookings, and give you full control with no third-party fees.",
+  keywords:
+    "direct booking website, property rentals, vacation rental websites, BookableSTR",
+  openGraph: {
+    title: "BookableSTR - Direct Booking Websites",
+    description:
+      "BookableSTR helps property owners build stunning websites for direct bookings without third-party fees.",
+    url: "https://bookablestr.com",
+    type: "website",
+  },
 };
 
 interface RootLayoutProps {
@@ -27,6 +37,26 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:type" content="website" />
+        {/* <meta property="og:image" content={metadata.openGraph.images[0].url} /> */}
+        {/* <meta
+          property="og:image:alt"
+          content={metadata.openGraph.images[0].alt}
+        /> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        {/* <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta name="twitter:description" content={metadata.twitter.description} />
+        <meta name="twitter:image" content={metadata.twitter.images[0]} /> */}
+      </Head>
       <body className={`${quicksand.variable} ${nunito.variable} antialiased`}>
         {children}
         <Footer />
